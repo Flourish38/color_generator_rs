7f4bd7562f7516f18a4e7c6fc18a569721486d1a
brsan <brsandvik@gmail.com>
2024-01-31 11:26:30 -0800

    Add lookup table generation

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,23 @@
+use std::time::Instant;
+use itertools::iproduct;
 use fast_srgb8::srgb8_to_f32;
 use indicatif::{ProgressBar, ProgressStyle};
 
 #[allow(non_camel_case_types)]
 type sRGB = [u8; 3];
-#[allow(non_camel_case_types)]
-// Bastardized name. ARGB in sRGB space.
-type sARGB = u32;
+
+fn as_index(c: sRGB) -> usize {
+    // RGB order. Might change later.
+    let mut out: usize = c[2] as usize;
+    out |= (c[1] as usize) << 8;
+    out |= (c[0] as usize) << 16;
+    out
+}
 
 struct RGB {
     r:f32,
     g:f32,
     b:f32
-}
-
-impl From<sARGB> for RGB {
-    fn from(c: sARGB) -> Self {
-        let r = (c >> 16) & 0xFF;
-        let g = (c >> 8) & 0xFF;
-        let b = c & 0xFF;
-
-        RGB{
-            r: srgb8_to_f32(r as u8),
-            g: srgb8_to_f32(g as u8),
-            b: srgb8_to_f32(b as u8)
-        }
-    }
 }
 
 impl From<sRGB> for RGB {
@@ -38,6 +31,7 @@ impl From<sRGB> for RGB {
 }
 
 #[allow(non_snake_case)]
+#[derive(PartialEq, Debug, Clone, Copy, Default)]
 struct Oklab {
     L: f32,
     a: f32,
@@ -62,12 +56,6 @@ impl From<RGB> for Oklab {
     }
 }
 
-impl From<sARGB> for Oklab {
-    fn from(c: sARGB) -> Self {
-        Into::<RGB>::into(c).into()
-    }
-}
-
 impl From<sRGB> for Oklab {
     fn from(c: sRGB) -> Self {
         Into::<RGB>::into(c).into()
@@ -79,44 +67,46 @@ fn HyAB (c1: &Oklab, c2: &Oklab) -> f32 {
     return (c1.L - c2.L).abs() + ((c1.a - c2.a).powi(2) + (c1.b - c2.b).powi(2)).sqrt()
 }
 
-fn main() {
-    let mut max_diff = 0.0;
-    let mut best_1: sRGB = [0, 0, 0];
-    let mut best_2: sRGB = [0, 0, 0];
-    let bar = ProgressBar::new(1 << 16).with_style(ProgressStyle::with_template("{elapsed_precise}/{duration_precise} {wide_bar} {percent:>02}% {pos}/{len} {per_sec}").unwrap());
-    for r in 0x00..=0xFF {
-        for b in 0x00..=0xFF {
-            let mut best_diff = 0.0;
-            let c1: sRGB = [r, 0xFF, b];
-            bar.inc(1);
-            let c1_lab: Oklab = c1.into();
-            for r2 in 0x00..=0xFF {
-                for b2 in 0x00..=0xFF {
-                    let c2 = [r2, 0, b2];
-                    let c2_lab: Oklab = c2.into();
 
-                    let diff = HyAB(&c1_lab, &c2_lab);
-                    
-                    if diff > best_diff {
-                        best_diff = diff;
-                        
-                        if diff > max_diff {
-                            max_diff = diff;
-                            best_1 = c1;
-                            best_2 = c2;
-                        }
-                        
-                    }
-                }
-                
-                //println!("{}\t{}\t{}\t\t{}", r, 0xFF, b, best_diff)
-            }
+struct SrgbLut<T> {
+    data: Vec<T>
+}
+
+impl<T: Copy> SrgbLut<T> {
+    fn new(f: impl Fn(sRGB) -> (T)) -> Self {
+        let mut data = Vec::with_capacity(1<<24);
+        // I wish there was an easy way to allow this to be parallel,
+        // But it is fast enough that it isn't a significant issue.
+        for (r, g, b) in iproduct!(0x00..=0xFF, 0x00..=0xFF, 0x00..=0xFF) {
+            let c = [r, g, b];
+            data.push(f(c))
         }
+        Self{ data: data }
     }
-    
 
-    
-    
+    fn get(&self, c: sRGB) -> T {
+        self.data[as_index(c)]
+    }
+}
 
-    println!("{:?}\t{:?}\t\t{}", best_1, best_2, HyAB(&best_1.into(), &best_2.into()))
+fn main() {
+    let mut t_start = Instant::now();
+    let oklab_lut: SrgbLut<Oklab> = SrgbLut::new(|c| c.into());
+    println!("{:#?}", t_start.elapsed());
+    //let bar = ProgressBar::new(1000).with_style(ProgressStyle::with_template("{elapsed_precise}/{duration_precise} {wide_bar} {percent:>02}% {pos}/{len} {per_sec}").unwrap());
+    let ok_ref: Oklab = [0, 0, 0].into();
+    t_start = Instant::now();
+    for _ in 0..10000000 {
+        let c: sRGB = rand::random();
+        let ok = oklab_lut.get(c);
+        let dist = HyAB(&ok, &ok_ref);
+    }
+    println!("{:#?}", t_start.elapsed());
+    t_start = Instant::now();
+    for _ in 0..10000000 {
+        let c: sRGB = rand::random();
+        let ok: Oklab = c.into();
+        let dist = HyAB(&ok, &ok_ref);
+    }
+    println!("{:#?}", t_start.elapsed());
 }
