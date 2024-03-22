b1018c9910609f31fb99d0409328e21e940fbafd
brsan <brsandvik@gmail.com>
2024-03-22 17:36:10 -0400

    implement pair distance metric, tidy interface

--- lib/src/metric.rs
+++ lib/src/metric.rs
@@ -38,72 +38,72 @@ impl SrgbLut<f32> {
     }
 }
 
-pub trait ScoreMetric {
-    fn get_min_score(&self) -> (usize, usize, f32);
+pub trait ScoreMetric<T: ScoreIndex> {
+    fn get_min_score(&self) -> (f32, T);
 
-    fn update(&mut self, i: usize, color: sRGB);
+    fn update(&mut self, updated_index: usize, updated_color: &sRGB);
 
-    fn test_improvement(&self, i: usize, updated_index: usize, updated_color: &sRGB) -> bool;
+    fn test_improvement(&self, updated_index: usize, updated_color: &sRGB) -> bool;
 }
 
-pub struct ConstrainedDistance<'a, 'b> {
-    color_lut: &'a SrgbLut<Oklab>,
-    pre_colors: Vec<Oklab>,
-    constraint_lut: &'b SrgbLut<f32>,
-    pre_constraints: Vec<f32>,
-    scores: Vec<(usize, f32)>,
-}
-// TODO: implement keyed priority queue - ish thing for storing scores.
-// It is definitely the correct data structure.
+// pub struct ConstrainedDistance<'a, 'b> {
+//     color_lut: &'a SrgbLut<Oklab>,
+//     pre_colors: Vec<Oklab>,
+//     constraint_lut: &'b SrgbLut<f32>,
+//     pre_constraints: Vec<f32>,
+//     scores: Vec<(usize, f32)>,
+// }
+// // TODO: implement keyed priority queue - ish thing for storing scores.
+// // It is definitely the correct data structure.
 
-impl<'a, 'b> ConstrainedDistance<'a, 'b> {
-    pub fn new(
-        colors: &Vec<sRGB>,
-        color_lut: &'a SrgbLut<Oklab>,
-        constraint_lut: &'b SrgbLut<f32>,
-    ) -> ConstrainedDistance<'a, 'b> {
-        let pre_colors = colors.iter().map(|c| color_lut.get(c)).collect_vec();
-        let pre_constraints = colors.iter().map(|c| constraint_lut.get(c)).collect_vec();
-        let scores = get_scores_constrained(&pre_colors, &pre_constraints, &HyAB);
-        ConstrainedDistance {
-            color_lut: color_lut,
-            pre_colors: pre_colors,
-            constraint_lut: constraint_lut,
-            pre_constraints: pre_constraints,
-            scores: scores,
-        }
-    }
-}
+// impl<'a, 'b> ConstrainedDistance<'a, 'b> {
+//     pub fn new(
+//         colors: &Vec<sRGB>,
+//         color_lut: &'a SrgbLut<Oklab>,
+//         constraint_lut: &'b SrgbLut<f32>,
+//     ) -> ConstrainedDistance<'a, 'b> {
+//         let pre_colors = colors.iter().map(|c| color_lut.get(c)).collect_vec();
+//         let pre_constraints = colors.iter().map(|c| constraint_lut.get(c)).collect_vec();
+//         let scores = get_scores_constrained(&pre_colors, &pre_constraints, &HyAB);
+//         ConstrainedDistance {
+//             color_lut: color_lut,
+//             pre_colors: pre_colors,
+//             constraint_lut: constraint_lut,
+//             pre_constraints: pre_constraints,
+//             scores: scores,
+//         }
+//     }
+// }
 
-impl<'a, 'b> ScoreMetric for ConstrainedDistance<'a, 'b> {
-    fn get_min_score(&self) -> (usize, usize, f32) {
-        get_min_score(&self.scores)
-    }
+// impl<'a, 'b> ScoreMetric<(usize, usize)> for ConstrainedDistance<'a, 'b> {
+//     fn get_min_score(&self) -> (usize, usize, f32) {
+//         get_min_score(&self.scores)
+//     }
 
-    fn update(&mut self, i: usize, color: sRGB) {
-        self.pre_colors[i] = self.color_lut.get(&color);
-        self.pre_constraints[i] = self.constraint_lut.get(&color);
-        update_scores_constrained(
-            &mut self.scores,
-            i,
-            &self.pre_colors,
-            &self.pre_constraints,
-            &HyAB,
-        );
-    }
+//     fn update(&mut self, i: usize, color: sRGB) {
+//         self.pre_colors[i] = self.color_lut.get(&color);
+//         self.pre_constraints[i] = self.constraint_lut.get(&color);
+//         update_scores_constrained(
+//             &mut self.scores,
+//             i,
+//             &self.pre_colors,
+//             &self.pre_constraints,
+//             &HyAB,
+//         );
+//     }
 
-    fn test_improvement(&self, i: usize, updated_index: usize, updated_color: &sRGB) -> bool {
-        let (j, prev_score) = self.scores[i];
-        if i == j {
-            return self.constraint_lut.get(&updated_color) > prev_score;
-        }
-        let new_color = self.color_lut.get(&updated_color);
-        let other_index = if i == updated_index { j } else { i };
-        let other_color = self.pre_colors[other_index];
-        let new_score = HyAB(&new_color, &other_color);
-        return new_score > prev_score;
-    }
-}
+//     fn test_improvement(&self, i: usize, updated_index: usize, updated_color: &sRGB) -> bool {
+//         let (j, prev_score) = self.scores[i];
+//         if i == j {
+//             return self.constraint_lut.get(&updated_color) > prev_score;
+//         }
+//         let new_color = self.color_lut.get(&updated_color);
+//         let other_index = if i == updated_index { j } else { i };
+//         let other_color = self.pre_colors[other_index];
+//         let new_score = HyAB(&new_color, &other_color);
+//         return new_score > prev_score;
+//     }
+// }
 
 // pub struct ConstraintOnly<'a> {
 //     constraint_lut: &'a SrgbLut<f32>,
@@ -138,33 +138,99 @@ impl<'a, 'b> ScoreMetric for ConstrainedDistance<'a, 'b> {
 //     }
 // }
 
-pub struct ConstraintOnly<'a> {
+pub struct Constraint<'a> {
     constraint_lut: &'a SrgbLut<f32>,
-    scores: Scores,
+    scores: Scores<usize>,
 }
 
-impl<'a> ConstraintOnly<'a> {
+impl<'a> Constraint<'a> {
     pub fn new(colors: &Vec<sRGB>, constraint_lut: &'a SrgbLut<f32>) -> Self {
         let data = colors.iter().map(|c| constraint_lut.get(c)).collect_vec();
-        ConstraintOnly {
+        Constraint {
             constraint_lut: constraint_lut,
             scores: Scores::new(&data),
         }
     }
 }
 
-impl<'a> ScoreMetric for ConstraintOnly<'a> {
-    fn get_min_score(&self) -> (usize, usize, f32) {
-        let (val, i) = self.scores.get_min_score();
-        (i, i, val)
+impl<'a> ScoreMetric<usize> for Constraint<'a> {
+    fn get_min_score(&self) -> (f32, usize) {
+        self.scores.get_min_score()
     }
 
-    fn update(&mut self, i: usize, color: sRGB) {
-        self.scores.update(i, self.constraint_lut.get(&color));
+    fn update(&mut self, updated_index: usize, updated_color: &sRGB) {
+        self.scores
+            .update(updated_index, self.constraint_lut.get(updated_color));
     }
 
-    fn test_improvement(&self, i: usize, updated_index: usize, updated_color: &sRGB) -> bool {
-        let _ = (i, updated_index);
+    fn test_improvement(&self, _updated_index: usize, updated_color: &sRGB) -> bool {
         self.constraint_lut.get(updated_color) > self.scores.get_min_score().0
+    }
+}
+
+pub struct PairDistance<'a> {
+    color_lut: &'a SrgbLut<Oklab>,
+    pre_colors: Vec<Oklab>,
+    pre_scores: Vec<(f32, usize)>,
+    scores: Scores<(usize, usize)>,
+}
+
+impl<'a> PairDistance<'a> {
+    pub fn new(colors: &Vec<sRGB>, color_lut: &'a SrgbLut<Oklab>) -> Self {
+        let pre_colors = colors.iter().map(|c| color_lut.get(c)).collect_vec();
+        let pre_scores = get_pair_scores(&pre_colors);
+        let scores = Scores::new_pairs(&pre_scores);
+        Self {
+            color_lut: color_lut,
+            pre_colors: pre_colors,
+            pre_scores: pre_scores,
+            scores: scores,
+        }
+    }
+
+    fn update_pair_score(&mut self, i: usize) {
+        let (val, ind) = get_pair_score(i, &self.pre_colors);
+        self.pre_scores[i] = (val, ind);
+        self.scores.update((i, ind), val);
+    }
+}
+
+impl<'a> ScoreMetric<(usize, usize)> for PairDistance<'a> {
+    fn get_min_score(&self) -> (f32, (usize, usize)) {
+        self.scores.get_min_score()
+    }
+
+    fn update(&mut self, updated_index: usize, updated_color: &sRGB) {
+        let new_color = self.color_lut.get(updated_color);
+        self.pre_colors[updated_index] = new_color;
+
+        // Recompute scores of indexes before updated_index
+        for i in 0..updated_index {
+            let (prev_score, prev_index) = self.pre_scores[i];
+            let score = HyAB(&new_color, &self.pre_colors[i]);
+            if score < prev_score {
+                self.pre_scores[i] = (score, updated_index);
+                self.scores.update((i, updated_index), score)
+            } else if prev_index == updated_index {
+                // Have to recompute score for this element
+                self.update_pair_score(i);
+            } // else, no need to change it
+        }
+
+        // Recompute score of updated_index
+        if updated_index < self.pre_scores.len() {
+            self.update_pair_score(updated_index);
+        }
+    }
+
+    fn test_improvement(&self, updated_index: usize, updated_color: &sRGB) -> bool {
+        let new_color = self.color_lut.get(updated_color);
+        let (old_score, (i, j)) = self.scores.get_min_score();
+        let color = if updated_index == i {
+            self.pre_colors[j]
+        } else {
+            self.pre_colors[i]
+        };
+        return HyAB(&new_color, &color) > old_score;
     }
 }
--- lib/src/score.rs
+++ lib/src/score.rs
@@ -1,6 +1,6 @@
 use std::{f32::INFINITY, fmt::Debug};
 
-use itertools::enumerate;
+use crate::color::{HyAB, Oklab};
 
 fn parent(i: usize) -> usize {
     (i - 1) / 2
@@ -130,127 +130,147 @@ impl Scores<(usize, usize)> {
     }
 }
 
-pub fn get_score<T, F: Fn(&T, &T) -> f32>(i: usize, pre_colors: &Vec<T>, dist: F) -> (usize, f32) {
+pub fn get_pair_score(i: usize, pre_colors: &Vec<Oklab>) -> (f32, usize) {
     let c = &pre_colors[i];
-    let mut score = (i, INFINITY);
+    let mut score = (INFINITY, i);
     for j in (i + 1)..pre_colors.len() {
-        let dist = dist(c, &pre_colors[j]);
-        if dist < score.1 {
-            score = (j, dist);
+        let dist = HyAB(c, &pre_colors[j]);
+        if dist < score.0 {
+            score = (dist, j);
         }
     }
     return score;
 }
 
-pub fn get_scores<T, F: Fn(&T, &T) -> f32>(pre_colors: &Vec<T>, dist: &F) -> Vec<(usize, f32)> {
+pub fn get_pair_scores(pre_colors: &Vec<Oklab>) -> Vec<(f32, usize)> {
     let mut scores = Vec::with_capacity(pre_colors.len() - 1);
     for i in 0..(pre_colors.len() - 1) {
-        scores.push(get_score(i, pre_colors, dist));
+        scores.push(get_pair_score(i, pre_colors));
     }
     return scores;
 }
 
-pub fn get_score_constrained<T, F: Fn(&T, &T) -> f32>(
-    i: usize,
-    pre_colors: &Vec<T>,
-    constraint: f32,
-    dist: F,
-) -> (usize, f32) {
-    let c = &pre_colors[i];
-    let mut score = (i, constraint);
-    for j in (i + 1)..pre_colors.len() {
-        let dist = dist(c, &pre_colors[j]);
-        if dist < score.1 {
-            score = (j, dist);
-        }
-    }
-    return score;
-}
+// pub fn get_score<T, F: Fn(&T, &T) -> f32>(i: usize, pre_colors: &Vec<T>, dist: F) -> (usize, f32) {
+//     let c = &pre_colors[i];
+//     let mut score = (i, INFINITY);
+//     for j in (i + 1)..pre_colors.len() {
+//         let dist = dist(c, &pre_colors[j]);
+//         if dist < score.1 {
+//             score = (j, dist);
+//         }
+//     }
+//     return score;
+// }
 
-pub fn get_scores_constrained<T, F: Fn(&T, &T) -> f32>(
-    pre_colors: &Vec<T>,
-    pre_constraints: &Vec<f32>,
-    dist: &F,
-) -> Vec<(usize, f32)> {
-    let mut scores = Vec::with_capacity(pre_colors.len());
-    for i in 0..pre_colors.len() {
-        scores.push(get_score_constrained(
-            i,
-            pre_colors,
-            pre_constraints[i],
-            dist,
-        ));
-    }
-    return scores;
-}
+// pub fn get_scores<T, F: Fn(&T, &T) -> f32>(pre_colors: &Vec<T>, dist: &F) -> Vec<(usize, f32)> {
+//     let mut scores = Vec::with_capacity(pre_colors.len() - 1);
+//     for i in 0..(pre_colors.len() - 1) {
+//         scores.push(get_score(i, pre_colors, dist));
+//     }
+//     return scores;
+// }
 
-pub fn get_min_score(scores: &Vec<(usize, f32)>) -> (usize, usize, f32) {
-    let mut output = (0, 0, INFINITY);
-    for (i, (j, val)) in enumerate(scores) {
-        if val < &output.2 {
-            output = (i, *j, *val);
-        }
-    }
-    output
-}
+// pub fn get_score_constrained<T, F: Fn(&T, &T) -> f32>(
+//     i: usize,
+//     pre_colors: &Vec<T>,
+//     constraint: f32,
+//     dist: F,
+// ) -> (usize, f32) {
+//     let c = &pre_colors[i];
+//     let mut score = (i, constraint);
+//     for j in (i + 1)..pre_colors.len() {
+//         let dist = dist(c, &pre_colors[j]);
+//         if dist < score.1 {
+//             score = (j, dist);
+//         }
+//     }
+//     return score;
+// }
 
-pub fn update_scores<T, F: Fn(&T, &T) -> f32>(
-    scores: &mut Vec<(usize, f32)>,
-    updated_index: usize,
-    pre_colors: &Vec<T>,
-    dist: &F,
-) {
-    let c_updated = &pre_colors[updated_index];
+// pub fn get_scores_constrained<T, F: Fn(&T, &T) -> f32>(
+//     pre_colors: &Vec<T>,
+//     pre_constraints: &Vec<f32>,
+//     dist: &F,
+// ) -> Vec<(usize, f32)> {
+//     let mut scores = Vec::with_capacity(pre_colors.len());
+//     for i in 0..pre_colors.len() {
+//         scores.push(get_score_constrained(
+//             i,
+//             pre_colors,
+//             pre_constraints[i],
+//             dist,
+//         ));
+//     }
+//     return scores;
+// }
 
-    // Recompute scores of indexes before updated_index
-    for i in 0..updated_index {
-        let (prev_index, prev_score) = scores[i];
-        let score = dist(c_updated, &pre_colors[i]);
-        if score < prev_score {
-            scores[i] = (updated_index, score);
-        } else if prev_index == updated_index {
-            // Have to recompute score for this element
-            scores[i] = get_score(i, pre_colors, dist);
-        } // else, no need to change it
-    }
+// pub fn get_min_score(scores: &Vec<(usize, f32)>) -> (usize, usize, f32) {
+//     let mut output = (0, 0, INFINITY);
+//     for (i, (j, val)) in enumerate(scores) {
+//         if val < &output.2 {
+//             output = (i, *j, *val);
+//         }
+//     }
+//     output
+// }
 
-    // Recompute score of updated_index
-    if updated_index < scores.len() {
-        scores[updated_index] = get_score(updated_index, pre_colors, dist)
-    }
-}
+// pub fn update_scores<T, F: Fn(&T, &T) -> f32>(
+//     scores: &mut Vec<(usize, f32)>,
+//     updated_index: usize,
+//     pre_colors: &Vec<T>,
+//     dist: &F,
+// ) {
+//     let c_updated = &pre_colors[updated_index];
 
-pub fn update_scores_constrained<T, F: Fn(&T, &T) -> f32>(
-    scores: &mut Vec<(usize, f32)>,
-    updated_index: usize,
-    pre_colors: &Vec<T>,
-    pre_constraints: &Vec<f32>,
-    dist: &F,
-) {
-    let c_updated = &pre_colors[updated_index];
+//     // Recompute scores of indexes before updated_index
+//     for i in 0..updated_index {
+//         let (prev_index, prev_score) = scores[i];
+//         let score = dist(c_updated, &pre_colors[i]);
+//         if score < prev_score {
+//             scores[i] = (updated_index, score);
+//         } else if prev_index == updated_index {
+//             // Have to recompute score for this element
+//             scores[i] = get_score(i, pre_colors, dist);
+//         } // else, no need to change it
+//     }
 
-    // Recompute scores of indexes before updated_index
-    for i in 0..updated_index {
-        let (prev_index, prev_score) = scores[i];
-        let score = dist(c_updated, &pre_colors[i]);
-        if score < prev_score {
-            scores[i] = (updated_index, score);
-        } else if prev_index == updated_index {
-            // Have to recompute score for this element
-            scores[i] = get_score_constrained(i, pre_colors, pre_constraints[i], dist);
-        } // else, no need to change it
-    }
+//     // Recompute score of updated_index
+//     if updated_index < scores.len() {
+//         scores[updated_index] = get_score(updated_index, pre_colors, dist)
+//     }
+// }
 
-    // Recompute score of updated_index
-    if updated_index < scores.len() {
-        scores[updated_index] = get_score_constrained(
-            updated_index,
-            pre_colors,
-            pre_constraints[updated_index],
-            dist,
-        )
-    }
-}
+// pub fn update_scores_constrained<T, F: Fn(&T, &T) -> f32>(
+//     scores: &mut Vec<(usize, f32)>,
+//     updated_index: usize,
+//     pre_colors: &Vec<T>,
+//     pre_constraints: &Vec<f32>,
+//     dist: &F,
+// ) {
+//     let c_updated = &pre_colors[updated_index];
+
+//     // Recompute scores of indexes before updated_index
+//     for i in 0..updated_index {
+//         let (prev_index, prev_score) = scores[i];
+//         let score = dist(c_updated, &pre_colors[i]);
+//         if score < prev_score {
+//             scores[i] = (updated_index, score);
+//         } else if prev_index == updated_index {
+//             // Have to recompute score for this element
+//             scores[i] = get_score_constrained(i, pre_colors, pre_constraints[i], dist);
+//         } // else, no need to change it
+//     }
+
+//     // Recompute score of updated_index
+//     if updated_index < scores.len() {
+//         scores[updated_index] = get_score_constrained(
+//             updated_index,
+//             pre_colors,
+//             pre_constraints[updated_index],
+//             dist,
+//         )
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
