593ffd6877215fe9965d92f41747d7d4644e9e55
brsan <brsandvik@gmail.com>
2024-03-22 20:05:03 -0400

    remove most comments

--- palette-optimizer/src/main.rs
+++ palette-optimizer/src/main.rs
@@ -14,29 +14,11 @@ fn breakpoint() {
 }
 
 fn main() {
-    // let c1: sRGB = [0x00, 0x10, 0x0D];
-    // let c2: sRGB = [0x03, 0x00, 0x05];
-    // let r1: RGB = c1.into();
-    // let r2: RGB = c2.into();
-    // let ok1 = c1.into();
-    // let ok2 = c2.into();
-    // println!("{:?}\t{:?}\t{}", ok1, ok2, HyAB(&ok1, &ok2));
-    // let c2 = [0xfe, 0xff, 0x00].into();
-    // let mut max_dist = ([0x00, 0x00, 0x00], 0.0);
-    // for (r, g, b) in iproduct!(0x00..=0xFF, 0x00..=0xFF, 0x00..=0xFF) {
-    //     let c = [r, g, b];
-    //     let dist = HyAB(&c.into(), &c2);
-    //     if dist > max_dist.1 {
-    //         max_dist = (c, dist);
-    //     }
-    // }
-    // println!("{}\t{}", max_dist.1, to_string(&max_dist.0))
-    //    let bgs = [[0x00, 0x00, 0x00], [0xFF, 0xFF, 0xFF]];
+    // let bgs = [[0x00, 0x00, 0x00], [0xFF, 0xFF, 0xFF]];
     // let backgrounds = bgs.iter().map(|c| (*c).into()).collect_vec();
     let color_lut = SrgbLut::new(|c| c.into());
     // let constraint_lut =
     //     SrgbLut::new_constraint(&backgrounds, |c1, c2| HyAB(c1, &color_lut.get(c2)));
-    // println!("{}\t{}", constraint_lut.get(&[0xff, 0xff, 0xff]), constraint_lut.get(&[0x00, 0x00, 0x00]));
     // let apca_constraint_lut = SrgbLut::new_constraint(&bgs.to_vec(), |c1, c2| APCA(c2, c1));
 
     let num_iter: u64 = 1000000000;
@@ -85,15 +67,5 @@ fn main() {
         // 's/[\[" #]//g'
     }
 
-    // let oklab_best = best.1.iter().map(|c| From::from(*c)).collect_vec();
-    // let best_scores = get_scores(&oklab_best, &HyAB);
-    // let min_score = get_min_score(&best_scores);
-    // println!(
-    //     "{}\t{}\t{}",
-    //     min_score.2,
-    //     to_string(&best.1[min_score.0]),
-    //     to_string(&best.1[min_score.1])
-    // );
-    // assert_eq!(best.0, min_score.2)
     // breakpoint();
 }
