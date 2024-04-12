bda4b5a2ef79b7e0812c44cc9ba8259ef90e58db
Flourish38 <emisandvik@gmail.com>
2024-04-12 14:23:07 -0700

    made the number of colors per ring calculator

--- palette-visualizer/src/main.rs
+++ palette-visualizer/src/main.rs
@@ -1,5 +1,7 @@
 use regex::Regex;
 use std::env;
+use std::f64::consts::{PI, TAU};
+use std::f64::INFINITY;
 use svg::node::element::path::Data;
 use svg::node::element::Path;
 use svg::node::Value;
@@ -45,7 +47,7 @@ fn make_ring(radii: (f64, f64), start_angle: f64, colors: Vec<String>) -> Vec<Pa
             vec![colors[0].clone(), colors[0].clone()],
         );
     }
-    let angle_offset = std::f64::consts::TAU / n as f64;
+    let angle_offset = TAU / n as f64;
     colors
         .iter()
         .enumerate()
@@ -73,7 +75,7 @@ fn calculate_radius_frac(inner_ring: usize, outer_ring: usize, inside_frac: f64)
     (partial + outer_ring as f64).sqrt() / partial.sqrt()
 }
 
-fn calculate_radii(per_ring: Vec<usize>) -> Vec<f64> {
+fn calculate_radii(per_ring: &Vec<usize>) -> Vec<f64> {
     let mut acc = 1.0;
     let mut rad_fracs = Vec::new();
     for i in 1..per_ring.len() {
@@ -87,8 +89,75 @@ fn calculate_radii(per_ring: Vec<usize>) -> Vec<f64> {
         total /= frac;
         outputs.push(total);
     }
+    outputs.push(0.0);
     outputs.reverse();
     outputs
+}
+
+fn squareness_objective((inner_radius, outer_radius): (f64, f64), n: usize) -> f64 {
+    // squareness is the square root of the area divided by the chord length.
+    let squareness = (PI * (outer_radius * outer_radius - inner_radius * inner_radius) / n as f64)
+        .sqrt()
+        / (outer_radius - inner_radius);
+
+    // pass this through a function that makes deviations from 1 punished
+    // this function is chosen because f(1/x) = f(x) for x > 0
+    squareness.ln().abs()
+}
+
+fn mean_squareness(list: &Vec<usize>) -> f64 {
+    let radii = calculate_radii(&list);
+    let mut total = 0.0;
+    for i in 0..list.len() {
+        total += list[i] as f64 * squareness_objective((radii[i], radii[i + 1]), list[i]);
+    }
+    total / (list.iter().sum::<usize>() as f64)
+}
+
+fn add_to_list(
+    list: &Vec<usize>,
+    n_remaining: usize,
+    layers_remaining: usize,
+) -> (Vec<usize>, f64) {
+    if layers_remaining == 0 {
+        let mut inner_list = list.clone();
+        inner_list.push(n_remaining);
+        let sq = mean_squareness(&inner_list);
+        (inner_list, sq)
+    } else {
+        let biggest_size = *list.last().unwrap_or(&0);
+        let mut best_list = vec![];
+        let mut best_score = INFINITY;
+        for i in biggest_size..((n_remaining + layers_remaining) / (layers_remaining + 1)) {
+            let mut inner_list = list.clone();
+            inner_list.push(i);
+            let (new_list, score) = add_to_list(&inner_list, n_remaining - i, layers_remaining - 1);
+            if score < best_score {
+                best_list = new_list;
+                best_score = score;
+            }
+        }
+        (best_list, best_score)
+    }
+}
+
+fn optimize_layers(n: usize) -> (Vec<usize>, f64) {
+    let baseline_squareness = squareness_objective((0.0, RADIUS), n);
+    let mut best_result = vec![n];
+    let mut best_score = baseline_squareness;
+    let mut layers = 1;
+    loop {
+        //println!("{}\t{}:\t{:?}", layers, best_score, best_result);
+        let (list, score) = add_to_list(&vec![], n, layers);
+        if score < best_score {
+            best_result = list;
+            best_score = score;
+            layers += 1;
+        } else {
+            //println!("{}\t{}:\t{:?}", layers + 1, score, list);
+            return (best_result, best_score);
+        }
+    }
 }
 
 fn make_rings(colors: Vec<String>) -> Vec<Vec<Path>> {
@@ -106,5 +175,32 @@ fn main() {
 
     // svg::save("image.svg", &document).unwrap();
 
-    println!("{:?}", calculate_radii(vec![3, 9, 15]))
+    use std::time::Instant;
+    let start_time = Instant::now();
+
+    let mut prev_len = 0;
+    let mut prev_score = INFINITY;
+    let mut increasing = false;
+    for i in 1..171 {
+        let results = optimize_layers(i);
+        let len = results.0.len();
+        if len != prev_len {
+            println!("{}:\t{}\t{}\t{:?}", i, len, results.1, results.0);
+            prev_len = len;
+        }
+        if prev_score < results.1 && !increasing {
+            println!(
+                "{}:\t{}\t{}\t{:?}",
+                i - 1,
+                len,
+                prev_score,
+                optimize_layers(i - 1).0
+            );
+            increasing = true;
+        }
+        if prev_score > results.1 {
+            increasing = false;
+        }
+        prev_score = results.1;
+    }
 }
